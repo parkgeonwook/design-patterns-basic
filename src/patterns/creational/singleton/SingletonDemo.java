ecb5ebe0e679dc6edc1a48faeb69c49c5c44567e
parkgeonwook <rjsdnr6331@gmail.com>
2026-06-04 11:31:22 +0900

    feat: 싱글톤 패턴

---
+++ src/patterns/creational/singleton/SingletonDemo.java
@@ -0,0 +1,11 @@
+package patterns.creational.singleton;
+
+public class SingletonDemo {
+
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println(singleton1 == singleton2);
+    }
+}
